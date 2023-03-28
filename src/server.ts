92e215985a8a57c84ad5ea92dddacfb35671d695
ibrarjut <ibrar@blacklionapp.xyz>
2023-03-28 21:43:24 +0500

    heap added

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ const upload = multer({
   }
 });
 const app: Express = express();
-const port = 5500;
+const port = 80;
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors({
